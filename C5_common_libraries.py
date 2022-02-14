ed6382e2c68f057d8a5bc408e8d0c193dcc92d6e
pprathamesh7 <64551585+pprathamesh7@users.noreply.github.com>
2022-02-14 23:49:09 +0530

    Updates C5_common _libraries
    Removed unnecessary lists and dictionaries, also changed the Element _action method.

--- C5_common_libraries.py
+++ C5_common_libraries.py
@@ -3,48 +3,63 @@
 
 # In[1]:
 
-
-
-CPU_list = ['Core i9','Core i7','Core i5','Core 2','Celeron','Pentium','AMD Ryzen 7','AMD Ryzen 5','AMD Ryzen 3','AMD Ryzen','AMD Athlon','Intel Xeon']
-Memory_list = ['4GB','8GB','16GB','32GB','64GB']
-Storage_list = ['128GB','256GB','500GB','512GB','1TB','2TB']
+class EA:
+    """
+    Use this method to convert perform click, clear or read operation on web elements.
+    input parameters :
+    1) driver : selenium webdriver instance
+    2) action : action on the webelement in string form ('read','click','clear')
+    3) Dict : Input in the form of a dictionary where keys are name of the element and values are their respective xpaths
+    """
+    def Element_action(driver,action,Dict):
+        element_names = list(Dict.keys())
+        List_of_values = []
+        for e in element_names:
+            xpath = Dict[e]
+            #print(e ," : ", Dict[e])
+            if action == 'click':
+                driver.find_element_by_xpath(xpath).click()
+            if action == 'clear':
+                driver.find_element_by_xpath(xpath).clear()
+            if action == 'read':
+                try:
+                    Value = driver.find_element_by_xpath(xpath).text
+                    print(Value)
+                except as e:
+                    Value = 'Element not found :' + e
+            #
+            #Dict_2 creation key:e, value: Value
         
-Notebooks = ['ThinkPad','ThinkBook','IdeaPad', 'Legion']
-Desktops = ['ThinkCentre','IdeaCentre','Yoga']
-CatchPhrase = {'NB no office':'ノートパソコン Windows11 公式レノボ直販','NB with office':'ノートパソコン Windows11 公式レノボ直販 Office付き','DT no office':'デスクトップパソコン Windows11 公式レノボ直販','DT with office':'デスクトップパソコン Windows11 公式レノボ直販 Office付き','Gaming NB no office':'ゲーミングノートPC Windows11 公式レノボ直販','Gaming DT no office':'ゲーミングノートPC Windows11 公式レノボ直販 Office付き','Gaming DT no office':'デスクトップゲーミングPC Windows11 公式レノボ直販','Gaming DT with office':'デスクトップゲーミングPC Windows11 公式レノボ直販 Office付き','Tablet':'タブレット Android 公式レノボ直販'}
+            List_of_values.append(Value)
+        return List_of_values
+
+class Xlsx_to_CSV:
+    def Xlsx2CSV(excel_name,csv_name):
+        """
+        Use this method to convert xlsx to csv
+        input parameters:
+        1) excel_name : file path of the excel to be converted.
+        2) csv_name : file path of the new csv file.
+        """
+        import pandas as pd
+        import sys
+        #read_file = pd.read_excel(excel_name,encoding="utf8")
+        read_file = pd.read_excel(excel_name)
+        read_file.to_csv (csv_name, index = None, header=True,encoding ='utf-8-sig')
     
 
-class EA:      
-    def Element_action(driver,action,xpath):
-        if action == 'click':
-            driver.find_element_by_xpath(xpath).click()    
-        if action == 'clear':
-            driver.find_element_by_xpath(xpath).clear() 
-        if action == 'read':
-            text = driver.find_element_by_xpath(xpath).text
-            return text
-    
-def Xlsx_to_CSV(excel_name,csv_name):
-    """
-    Use this library to convert xlsx to csv
-    """
-    import pandas as pd
-    import sys
-    #read_file = pd.read_excel(excel_name,encoding="utf8")
-    read_file = pd.read_excel(excel_name)
-    read_file.to_csv (csv_name, index = None, header=True,encoding ='utf-8-sig')
-    
-    
-def FTP_File_Upload(FTP_HOST,FTP_USER,FTP_PASS,FTP_path,file):
-    """
-    Use this library to upload a file to FTP location
-    """
-    from ftplib import FTP
-    from pathlib import Path
+class FTP_File_Upload:
+    def File_Upload(FTP_HOST,FTP_USER,FTP_PASS,FTP_path,file):
+        """
+        Use this method to upload a file to FTP location.
+        
+        """
+        from ftplib import FTP
+        from pathlib import Path
 
-    with FTP(FTP_HOST, FTP_USER, FTP_PASS) as ftp, open(FTP_path, 'rb') as file:
-        ftp.encoding = "utf-8"
-        ftp.port = 21
-        ftp.storbinary(f'STOR {FTP_path.name}', file)
+        with FTP(FTP_HOST, FTP_USER, FTP_PASS) as ftp, open(FTP_path, 'rb') as file:
+            ftp.encoding = "utf-8"
+            ftp.port = 21
+            ftp.storbinary(f'STOR {FTP_path.name}', file)
         
 
